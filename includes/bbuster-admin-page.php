2600c8dbb866f0bc19405e6ea74c766801e5634c
Alvind <alvind.caesar@gmail.com>
2022-03-17 09:33:24 +0800

    Code fixes to adhere with WP.org plugin guidelines

--- includes/bbuster-admin-page.php
+++ includes/bbuster-admin-page.php
@@ -13,18 +13,14 @@ function bbuster_options_page()
       <?php settings_fields('bbuster_settings_group'); ?>
 
       <h4>Enable</h4>
+      
       <p>
-        <input name="bbuster_disable_auto_updates" id="bbbuster_disable_auto_updates" type="checkbox" value="1" <?php checked('1', $bbuster_options[0]); ?> />
-        <label for="bbuster_disable_auto_updates" class="description">Disable the Theme and Plugin Auto Updates</label>
-      </p>
-
-      <p>
-        <input name="bbuster_disable_emoji" id="bbuster_disable_emoji" type="checkbox" value="1" <?php checked('1', $bbuster_options[1]); ?> />
+        <input name="bbuster_disable_emoji" id="bbuster_disable_emoji" type="checkbox" value="1" <?php checked('1', $bbuster_options[0]); ?> />
         <label for="bbuster_disable_emoji" class="description">Remove Emoji Scripts</label>
       </p>
 
       <p>
-        <input name="bbuster_disable_fse_global_styles" id="bbuster_disable_fse_global_styles" type="checkbox" value="1" <?php checked('1', $bbuster_options[2]); ?> />
+        <input name="bbuster_disable_fse_global_styles" id="bbuster_disable_fse_global_styles" type="checkbox" value="1" <?php checked('1', $bbuster_options[1]); ?> />
         <label for="bbuster_disable_fse_global_styles" class="description">Remove Full Site Editing Global Styles</label>
       </p>
 
@@ -47,7 +43,6 @@ add_action('admin_menu', 'bbuster_add_options_link');
 
 function bbuster_register_settings()
 {
-  register_setting('bbuster_settings_group', 'bbuster_disable_auto_updates');
   register_setting('bbuster_settings_group', 'bbuster_disable_emoji');
   register_setting('bbuster_settings_group', 'bbuster_disable_fse_global_styles');
 }
