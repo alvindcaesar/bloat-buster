339c041353b55214eaca5c5df1f16cbe9c65e4ee
Alvind <alvind.caesar@gmail.com>
2022-03-24 15:02:52 +0800

    :recycle: Total code refactor - OOP-ised

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '263ddd8ac70b0ccf5d688640a32b671efbd27e46',
+        'name' => 'alvindcaesar/bloat-buster',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alvindcaesar/bloat-buster' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '263ddd8ac70b0ccf5d688640a32b671efbd27e46',
+            'dev_requirement' => false,
+        ),
+    ),
+);
