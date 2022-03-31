093d62373bd47accb15429f842b8f5617db63507
Alvind <alvind.caesar@gmail.com>
2022-03-31 12:15:05 +0800

    :rocket: Version 1.1

---
+++ includes/class-setting-page.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @package BloatBuster
+ */
+
+
+/**
+* Undocumented class
+*/
+
+ class Setting_Page
+ {
+    public function __construct() {
+      add_action('admin_menu', array( $this, 'add_options_link'));
+    }
+
+    public function add_options_link() {
+      add_options_page('Bloat Buster Options', 'Bloat Buster', 'manage_options', 'bbuster-options', array( $this, 'options_page' ));
+    }
+
+    public function options_page() {
+      require_once PLUGIN_PATH . 'includes/partials/setting.php';
+    }
+ }
