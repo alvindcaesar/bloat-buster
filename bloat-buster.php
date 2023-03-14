b5f28e360d1247154f06902cd04dbf843974a11e
Alvind <alvind.caesar@gmail.com>
2023-03-14 14:42:37 +0800

    `v1.2`

--- bloat-buster.php
+++ bloat-buster.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Bloat Buster
  * Plugin URI:        https://github.com/alvindcaesar/bloat-buster
  * Description:       A super simple plugin to remove bloat from your WordPress site.
- * Version:           1.1.2.1
+ * Version:           1.2
  * Author:            Alvind Caesar
  * Author URI:        https://alvindcaesar.com
  * License:           GPL-2.0+
@@ -18,46 +18,18 @@
  * Domain Path:       /languages
  */
 
+defined("WPINC") or die;
 
-if ( ! defined('ABSPATH') ) {
-  exit;
+if (file_exists( dirname(__FILE__) . '/vendor/autoload.php' ) ) {
+  require_once dirname(__FILE__) . '/vendor/autoload.php';
 }
 
-if( ! class_exists('Bloat_Buster'))
-{
-  class Bloat_Buster
-  {
-    private static $instance;
+define( 'BBUSTER_PATH', plugin_dir_path(__FILE__));
+define( 'BBUSTER_URL',  plugin_dir_url(__FILE__));
+define( 'BBUSTER_FILE', plugin_basename(__FILE__));
+define( 'BBUSTER_NAME', 'bloat-buster');
+define( 'BBUSTER_VERSION', '1.2');
 
-    public static function instance()
-    {
-      if ( ! isset(self::$instance) && ! (self::$instance instanceof Bloat_Buster))
-      {
-        self::$instance = new Bloat_Buster();
-        self::$instance->define_constants();
-        self::$instance->includes();
-        self::$instance->init = new Bloat_Buster_Init();
-      }
-      return self::$instance;
-    }
-
-    private function define_constants()
-    {
-      define( 'BBUSTER_PATH', plugin_dir_path(__FILE__));
-      define( 'BBUSTER_URL',  plugin_dir_url(__FILE__));
-      define( 'BBUSTER_FILE', plugin_basename(__FILE__));
-      define( 'BBUSTER_NAME', 'bloat-buster');
-      define( 'BBUSTER_VERSION', '1.1.2.1');
-    }
-
-    private function includes()
-    {
-      foreach ( glob( BBUSTER_PATH . "includes/*.php") as $class )
-      {
-        require_once $class;
-      }
-    }
-  }
-  
-  add_action( 'plugins_loaded', array( 'Bloat_Buster', 'instance') );
+if (class_exists('Bloatbuster\\Init')) {
+  Bloatbuster\Init::register_services();
 }
