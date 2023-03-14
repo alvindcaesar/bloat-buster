1d28bf23e1b33570f92004e2dc9a2b24be64d86a
Alvind <alvind.caesar@gmail.com>
2023-03-14 11:22:42 +0800

    :recycle: Use composer PSR-4 convention autoloader

---
+++ src/Pages/Settings/Register.php
@@ -0,0 +1,34 @@
+<?php
+namespace Bloatbuster\Pages\Settings;
+
+class Register
+{
+  public function register()
+  {
+    add_action('admin_init', array( $this, 'register_setting'));
+  }
+
+  public function register_setting() 
+  {
+    $settings = $this->settings();
+
+    foreach ($settings as $setting) {
+      register_setting('bloat_buster', '_bbuster_'. $setting );
+    }
+  }
+
+  public function settings() 
+  {
+    return array(
+      'disable_emoji',
+      'disable_fse_global_styles',
+      'remove_rsd_link',
+      'remove_shortlink',
+      'disable_embed',
+      'disable_xmlrpc',
+      'hide_wp_version',
+      'disable_heartbeat',
+      'dequeue_dashicon'
+    );
+  }
+}
