a7d5eab9cf4f7c12408ce2a186b228d1da6a613c
Alvind <alvind.caesar@gmail.com>
2023-03-15 23:34:31 +0800

    Feature: Export settings

---
+++ src/Pages/Settings/Export.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Bloatbuster\Pages\Settings;
+
+class Export
+{
+  public function register()
+  {
+    add_action('admin_post_export_settings', array($this, 'export_settings'));
+  }
+
+  public function export_settings()
+   {
+    $settings_name = \Bloatbuster\Pages\Settings\Register::settings_name();
+
+    $settings = array();
+
+    foreach ($settings_name as $setting) {
+      $settings['_bbuster_'. $setting] = get_option('_bbuster_'. $setting);
+    }
+
+    $json_data = json_encode($settings); // Encode the settings as JSON
+
+    header('Content-disposition: attachment; filename=bloatbuster_settings_export.json'); // Set the filename for the downloaded file
+
+    header('Content-Type: application/json'); // Set the content type to JSON
+
+    echo $json_data; // Output the JSON data
+
+    exit;
+   }
+}
