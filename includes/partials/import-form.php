0d223bf1f60bd32d3bfe2afef736f4b674a5960e
Alvind <alvind.caesar@gmail.com>
2024-01-06 11:54:08 +0800

    Enhance import form functionality and user experience

--- includes/partials/import-form.php
+++ includes/partials/import-form.php
@@ -1,9 +1,9 @@
 <h4>Import</h4>
-<form method="post" enctype="multipart/form-data" action="<?php echo admin_url( 'admin-post.php' ); ?>">
+<form method="post" enctype="multipart/form-data" action="<?php echo admin_url( 'admin-post.php' ); ?>" id="importForm">
   <input type="hidden" name="action" value="import_settings">
-  <input type="file" name="json_file" id="json_file">
+  <input type="file" name="json_file" id="json_file" onchange="checkFile()">
   <?php wp_nonce_field( 'import_settings' ); ?>
-  <button type="submit" class="button-secondary">Import Settings</button>
+  <button type="submit" class="button-secondary" id="submitButton" disabled>Import Settings</button>
 </form>
 
 <p class="description">To import your Bloat Buster settings from a JSON file, follow these steps:</p>
@@ -14,3 +14,11 @@
   <li>Once the import is complete, your Bloat Buster settings will be automatically updated.</li>
 </ol>
 <p>Please note that importing settings from a JSON file will overwrite your current Bloat Buster settings, so make sure to back up your current settings before importing.</p>
+
+<script>
+  function checkFile() {
+    var fileInput = document.getElementById('json_file');
+    var submitButton = document.getElementById('submitButton');
+    submitButton.disabled = !fileInput.files.length;
+  }
+</script>
